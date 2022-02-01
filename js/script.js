665e1ceac9cb00e541b1b3a860d900d7e39f1bd5
pablolfbs <pablo.fariabritto@gmail.com>
2022-02-01 08:59:23 -0300

    Atualizações

--- js/script.js
+++ js/script.js
@@ -6,49 +6,42 @@ const calculoInvestimento = () => {
 
     const investimentoMensal =
         parseFloat(document.querySelector(".investimento-mensal").value) > 0 ?
-        parseFloat(document.querySelector(".investimento-mensal").value) : 0;
+        parseFloat(document.querySelector(".investimento-mensal").value) : 0
     const investimentoInicial =
         parseFloat(document.querySelector(".investimento-inicial").value) > 0 ?
-        parseFloat(document.querySelector(".investimento-inicial").value) :
-        0;
-    const periodo = parseInt(document.querySelector(".periodo").value);
-    const tx = parseFloat(document.querySelector(".tx").value / 100);
-    let montante = investimentoInicial * Math.pow(1 + tx, periodo);
+        parseFloat(document.querySelector(".investimento-inicial").value) : 0
+    const periodo =
+        document.querySelector(".periodo").value < 720 ? parseInt(document.querySelector(".periodo").value) : alert('Período não pode ser maior que 720 meses')
+    const tx = parseFloat(document.querySelector(".tx").value / 100)
+    let montante = investimentoInicial * Math.pow(1 + tx, periodo)
 
     for (let i = 0; i < periodo; i++) {
-        montante += investimentoMensal * Math.pow(1 + tx, i);
+        montante += investimentoMensal * Math.pow(1 + tx, i)
     }
 
-    const valorInvestido = investimentoMensal * periodo + investimentoInicial;
-    const rendimento = montante - valorInvestido;
+    const valorInvestido = investimentoMensal * periodo + investimentoInicial
+    const rendimento = montante - valorInvestido
 
     if (montante) {
         document.querySelector("#valor-investido").innerHTML = moedaFormatada(valorInvestido);
         document.querySelector("#rendimento").innerHTML = moedaFormatada(rendimento);
         document.querySelector("#montante").innerHTML = moedaFormatada(montante);
 
-        for (const item of itensHidden) {
-            item.removeAttribute("hidden");
-        }
+        for (const item of itensHidden) item.removeAttribute("hidden")
     }
-};
+}
 
-document.querySelector("button").onclick = calculoInvestimento;
+document.querySelector("button").onclick = calculoInvestimento
 
-const moedaFormatada = valor =>
-    parseFloat(valor).toLocaleString("pt-br", {
-        style: "currency",
-        currency: "BRL",
-    });
+const moedaFormatada = valor => parseFloat(valor).toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 
 for (const item of itensInput) {
-    item.addEventListener("input", e => e.currentTarget.value = e.currentTarget.value
-        .replace(/[^0-9.]/g, "")
-        .replace(/(\..*?)\..*/g, "$1")
-    );
+    item.addEventListener("input", e => e.currentTarget.value = e.currentTarget.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1"));
 }
 
 document.querySelector("#limpar").addEventListener("click", () => {
     for (const item of itensInput) item.value = "";
     document.querySelector('#container-resultado').setAttribute('hidden', true)
-});
+})
+
+document.addEventListener("keyup", e => { if (e.key === 'Enter') calculoInvestimento() })
