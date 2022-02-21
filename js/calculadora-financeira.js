794aa1f23154feefda9d747f3d70a7c0b0e41dbb
pablolfbs <pablo.fariabritto@gmail.com>
2022-02-20 22:04:26 -0300

    Implementação cotação de moedas

---
+++ js/calculadora-financeira.js
@@ -0,0 +1,44 @@
+const itensInput = document.querySelectorAll("input")
+const itensHidden = document.querySelectorAll("div[hidden]")
+
+const calculoInvestimento = () => {
+    // M = C (1+i)^t
+
+    const investimentoMensal =
+        parseFloat(document.querySelector(".investimento-mensal").value) > 0 ? parseFloat(document.querySelector(".investimento-mensal").value) : 0
+    const investimentoInicial =
+        parseFloat(document.querySelector(".investimento-inicial").value) > 0 ? parseFloat(document.querySelector(".investimento-inicial").value) : 0
+    const periodo = document.querySelector(".periodo").value < 720 ? parseInt(document.querySelector(".periodo").value) : alert('Período não pode ser maior que 720 meses')
+    const tx = parseFloat(document.querySelector(".tx").value / 100)
+    let montante = investimentoInicial * Math.pow(1 + tx, periodo)
+
+    for (let i = 0; i < periodo; i++) {
+        montante += investimentoMensal * Math.pow(1 + tx, i)
+    }
+
+    const valorInvestido = investimentoMensal * periodo + investimentoInicial
+    const rendimento = montante - valorInvestido
+
+    if (montante) {
+        document.querySelector("#valor-investido").innerHTML = moedaFormatada(valorInvestido)
+        document.querySelector("#rendimento").innerHTML = moedaFormatada(rendimento)
+        document.querySelector("#montante").innerHTML = moedaFormatada(montante)
+
+        for (const item of itensHidden) item.removeAttribute("hidden")
+    }
+}
+
+document.querySelector("button").onclick = calculoInvestimento
+
+const moedaFormatada = valor => parseFloat(valor).toLocaleString("pt-br", { style: "currency", currency: "BRL" })
+
+for (const item of itensInput) {
+    item.addEventListener("input", e => e.currentTarget.value = e.currentTarget.value.replace(/[^0-9.]/g, "").replace(/(\..*?)\..*/g, "$1"))
+}
+
+document.querySelector("#limpar").addEventListener("click", () => {
+    for (const item of itensInput) item.value = ""
+    document.querySelector('#container-resultado').setAttribute('hidden', true)
+})
+
+document.addEventListener("keyup", e => { if (e.key === 'Enter') calculoInvestimento() })
---
+++ js/cotacao-moedas.js
@@ -0,0 +1,98 @@
+const loadValues = async() => {
+    const response = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,BTC-USD,GBP-BRL')
+    const values = await response.json()
+
+    const moedas = montaObjeto(values)
+
+    montaListaMoedas(moedas)
+}
+
+const montaListaMoedas = moedas => {
+    moedas.forEach(m => {
+        criaElementos(m)
+    })
+}
+
+const criaElementos = m => {
+    const valor = m.sigla == 'BTCUSD' ? valorFormatadoEmDolar(m.valor) : valorFormatadoEmReal(m.valor)
+    const variacao = m.variacao + ' $'
+
+    const position = document.getElementsByTagName('ul')[0]
+
+    const liEl = document.createElement('li')
+    const spanEl = document.createElement('span')
+    const abreParentesesEl = document.createElement('span')
+    const fechaParentesesEl = document.createElement('span')
+
+    const spanText = document.createTextNode(variacao)
+    const liText = document.createTextNode(m.nome + ': ' + valor)
+    const abreParentesesText = document.createTextNode('(')
+    const fechaParentesesText = document.createTextNode(')')
+
+    spanEl.style.color = corFonte(m, spanEl)
+
+    abreParentesesEl.appendChild(abreParentesesText)
+    fechaParentesesEl.appendChild(fechaParentesesText)
+    spanEl.appendChild(spanText)
+
+    liEl.appendChild(liText)
+    liEl.appendChild(abreParentesesEl)
+    liEl.appendChild(spanEl)
+    liEl.appendChild(fechaParentesesEl)
+
+    position.appendChild(liEl)
+}
+
+const corFonte = (m, spanEl) => {
+    if (m.variacao > 0)
+        return 'lightgreen'
+    if (m.variacao < 0)
+        return 'red'
+    else
+        return 'lightgrey'
+}
+
+const montaObjeto = values => {
+    return [{
+        nome: 'Dólar',
+        sigla: 'USDBRL',
+        valor: values['USDBRL'].bid,
+        variacao: values['USDBRL'].varBid
+    }, {
+        nome: 'Euro',
+        sigla: 'EURBRL',
+        valor: values['EURBRL'].bid,
+        variacao: values['EURBRL'].varBid
+    }, {
+        nome: 'Libra Esterlina',
+        sigla: 'GBPBRL',
+        valor: values['GBPBRL'].bid,
+        variacao: values['GBPBRL'].varBid
+    }, {
+        nome: 'Bitcoin',
+        sigla: 'BTCBRL',
+        valor: values['BTCBRL'].bid * 1000,
+        variacao: values['BTCBRL'].varBid
+    }, {
+        nome: 'Bitcoin',
+        sigla: 'BTCUSD',
+        valor: values['BTCUSD'].bid,
+        variacao: values['BTCUSD'].varBid
+    }]
+}
+
+const valorFormatadoEmReal = valor => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(valor)
+}
+
+const valorFormatadoEmDolar = valor => {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(valor)
+}
+
+loadValues()
