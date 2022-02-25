ac6d7f2cf34747c109bff9212e6d391026dc9155
pablolfbs <pablo.fariabritto@gmail.com>
2022-02-25 18:50:41 -0300

    refresh 5 segundos e inclusão de porcentagem

--- js/cotacao-moedas.js
+++ js/cotacao-moedas.js
@@ -16,16 +16,21 @@ const montaListaMoedas = moedas => {
 const criaElementos = m => {
     const valor = m.sigla == 'BTCUSD' ? valorFormatadoEmDolar(m.valor) : valorFormatadoEmReal(m.valor)
     const variacao = m.variacao + ' $'
+    const porcentagem = m.porcentagem + '%'
 
     const position = document.getElementsByTagName('ul')[0]
 
     const liEl = document.createElement('li')
-    const spanEl = document.createElement('span')
-    spanEl.style.color = corFonte(m, spanEl)
+    const spanVariacaoEl = document.createElement('span')
+    spanVariacaoEl.style.color = corFonte(m, spanVariacaoEl)
+    const spanPorcentagemEl = document.createElement('span')
+    spanPorcentagemEl.style.color = corFonte(m, spanPorcentagemEl)
     const abreParentesesEl = document.createElement('span')
     const fechaParentesesEl = document.createElement('span')
 
-    const spanText = document.createTextNode(variacao)
+    const spanVariacaoText = document.createTextNode(variacao)
+    const spanPorcentagemText = document.createTextNode(porcentagem)
+    spanPorcentagemEl.style.marginLeft = '5px'
     const liText = document.createTextNode(m.nome + ': ' + valor)
     const abreParentesesText = document.createTextNode('(')
     abreParentesesEl.style.marginLeft = '5px'
@@ -33,13 +38,16 @@ const criaElementos = m => {
 
     abreParentesesEl.appendChild(abreParentesesText)
     fechaParentesesEl.appendChild(fechaParentesesText)
+    spanVariacaoEl.appendChild(spanVariacaoText)
+    spanPorcentagemEl.appendChild(spanPorcentagemText)
 
-    spanEl.appendChild(spanText)
+    spanVariacaoEl.appendChild(spanVariacaoText)
 
     liEl.appendChild(liText)
     liEl.appendChild(abreParentesesEl)
-    liEl.appendChild(spanEl)
+    liEl.appendChild(spanVariacaoEl)
     liEl.appendChild(fechaParentesesEl)
+    liEl.appendChild(spanPorcentagemEl)
 
     position.appendChild(liEl)
 }
@@ -58,27 +66,32 @@ const montaObjeto = values => {
         nome: 'Dólar',
         sigla: 'USDBRL',
         valor: values['USDBRL'].bid,
-        variacao: values['USDBRL'].varBid
+        variacao: values['USDBRL'].varBid,
+        porcentagem: values['USDBRL'].pctChange
     }, {
         nome: 'Euro',
         sigla: 'EURBRL',
         valor: values['EURBRL'].bid,
-        variacao: values['EURBRL'].varBid
+        variacao: values['EURBRL'].varBid,
+        porcentagem: values['EURBRL'].pctChange
     }, {
         nome: 'Libra Esterlina',
         sigla: 'GBPBRL',
         valor: values['GBPBRL'].bid,
-        variacao: values['GBPBRL'].varBid
+        variacao: values['GBPBRL'].varBid,
+        porcentagem: values['GBPBRL'].pctChange
     }, {
         nome: 'Bitcoin',
         sigla: 'BTCBRL',
         valor: values['BTCBRL'].bid * 1000,
-        variacao: values['BTCBRL'].varBid
+        variacao: values['BTCBRL'].varBid,
+        porcentagem: values['BTCBRL'].pctChange
     }, {
         nome: 'Bitcoin',
         sigla: 'BTCUSD',
         valor: values['BTCUSD'].bid,
-        variacao: values['BTCUSD'].varBid
+        variacao: values['BTCUSD'].varBid,
+        porcentagem: values['USDBRL'].pctChange
     }]
 }
 
@@ -97,3 +110,7 @@ const valorFormatadoEmDolar = valor => {
 }
 
 loadValues()
+setInterval(() => {
+    document.getElementsByTagName('ul')[0].innerHTML = ''
+    loadValues()
+}, 5000);
