5fcbd949f795a6a99204cc70c321a9b83819ae1c
pablolfbs <pablo.fariabritto@gmail.com>
2022-03-27 20:35:51 -0300

    updates

---
+++ js/calculadora.js
@@ -0,0 +1,49 @@
+const zero = document.getElementById('0').innerHTML;
+const um = document.getElementById('1').innerHTML;
+const dois = document.getElementById('2').innerHTML;
+const tres = document.getElementById('3').innerHTML;
+const quatro = document.getElementById('4').innerHTML;
+const cinco = document.getElementById('5').innerHTML;
+const seis = document.getElementById('6').innerHTML;
+const sete = document.getElementById('7').innerHTML;
+const oito = document.getElementById('8').innerHTML;
+const nove = document.getElementById('9').innerHTML;
+const soma = document.getElementById('+').innerHTML;
+const subtracao = document.getElementById('-').innerHTML;
+const multiplicacao = document.getElementById('x').innerHTML;
+const divisao = document.getElementById('/').innerHTML;
+let ultima = ''
+let valor1 = ''
+let valor2 = ''
+
+document.getElementById('calculadora').addEventListener('click', e => {
+    e = e || window.event;
+    const target = e.target
+    const text = target.innerText;
+
+    let res = ''
+    let operador = ''
+
+    if (!isNaN(text) || text === ',') {
+        document.getElementById('resultado').value += text
+        if (valor1)
+            valor2 = document.getElementById('resultado').value
+        else
+            valor1 = document.getElementById('resultado').value
+    } else {
+        document.getElementById('span-calculo').innerHTML += document.getElementById('resultado').value + ' ' + text
+    }
+    if (ultima === '+') {
+        document.getElementById('resultado').value = ''
+        document.getElementById('resultado').value += text
+        fazCalculo()
+    }
+
+    ultima = text
+
+}, false)
+
+const fazCalculo = () => {
+    console.log(valor1 + valor2)
+    valor1 = ''
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 document.querySelector('#botao-cotacao').addEventListener('click', () => window.location.href = 'cotacao-moedas.html')
 
-document.querySelector('#botao-calculadora').addEventListener('click', () => window.location.href = 'calculadora-financeira.html')
+// document.querySelector('#botao-calculadora').addEventListener('click', () => window.location.href = 'calculadora.html')
+
+document.querySelector('#botao-calculadora-financeira').addEventListener('click', () => window.location.href = 'calculadora-financeira.html')
 
 document.querySelector('#botao-conversor').addEventListener('click', () => window.location.href = 'conversor-valores.html')
 
